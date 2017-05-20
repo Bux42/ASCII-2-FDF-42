8a00a38af4fc95c6d700a859de4494c2b7a4b173
Virgile DESVAUX <videsvau@e1r8p17.42.fr>
2017-05-20 02:36:54 +0200

    download fdf

--- script.js
+++ script.js
@@ -17,4 +17,27 @@ function convert() {
     		document.getElementById("comment fart").value = document.getElementById("comment fart").value + two;
     	i++;
     }
+    saveTextAsFile();
+}
+
+function saveTextAsFile() {
+  var textToWrite = document.getElementById('comment fart').value;
+  var textFileAsBlob = new Blob([ textToWrite ], { type: 'text/plain' });
+  var fileNameToSaveAs = "map.fdf";
+
+  var downloadLink = document.createElement("a");
+  downloadLink.download = fileNameToSaveAs;
+  downloadLink.innerHTML = "Download File";
+  if (window.webkitURL != null) {
+    // Chrome allows the link to be clicked without actually adding it to the DOM.
+    downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+  } else {
+    // Firefox requires the link to be added to the DOM before it can be clicked.
+    downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+    downloadLink.onclick = destroyClickedElement;
+    downloadLink.style.display = "none";
+    document.body.appendChild(downloadLink);
+  }
+
+  downloadLink.click();
 }
